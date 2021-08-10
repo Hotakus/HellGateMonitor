aa1d792c9351c523af91cfbf421a63093050d297
Hotakus <553070703@qq.com>
2021-08-10 13:39:24 +0800

    create lvgl

---
+++ Source/HgmLvgl/HgmLvgl.cpp
@@ -0,0 +1,1 @@
+#include "HgmLvgl.h"
---
+++ Source/HgmLvgl/HgmLvgl.h
@@ -0,0 +1,14 @@
+#ifndef HELLGATEMONITOR_HGMLVGL_H
+#define HELLGATEMONITOR_HGMLVGL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_HGMLVGL_H
