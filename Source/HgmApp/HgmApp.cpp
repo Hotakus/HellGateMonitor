74da24292c60b79be8b098f3e86c0df1508d215a
Hotakus <553070703@qq.com>
2021-08-12 05:20:36 +0800

    fix some bugs

---
+++ Source/HgmApp/HgmApp.cpp
@@ -0,0 +1,1 @@
+#include "HgmApp.h"
---
+++ Source/HgmApp/HgmApp.h
@@ -0,0 +1,14 @@
+#ifndef HELLGATEMONITOR_HGMAPP_H
+#define HELLGATEMONITOR_HGMAPP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_HGMAPP_H
