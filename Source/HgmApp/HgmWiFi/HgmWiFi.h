d0f82907d58bb5dd1414f7299b754d9d874d78b0
Hotakus <553070703@qq.com>
2021-08-12 19:42:57 +0800

    Complete the framework of the WiFi

---
+++ Source/HgmApp/HgmWiFi/HgmWiFi.h
@@ -0,0 +1,14 @@
+﻿#ifndef HELLGATEMONITOR_HGMWIFI_H
+#define HELLGATEMONITOR_HGMWIFI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_HGMWIFI_H
