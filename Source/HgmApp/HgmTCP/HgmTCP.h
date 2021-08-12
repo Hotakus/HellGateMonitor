174b1790347ca1e950a64d38adfee83214f13707
Hotakus <553070703@qq.com>
2021-08-13 01:07:09 +0800

    Finish the framework of the TCP

---
+++ Source/HgmApp/HgmTCP/HgmTCP.h
@@ -0,0 +1,14 @@
+﻿#ifndef HELLGATEMONITOR_HGMTCP_H
+#define HELLGATEMONITOR_HGMTCP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_HGMTCP_H
