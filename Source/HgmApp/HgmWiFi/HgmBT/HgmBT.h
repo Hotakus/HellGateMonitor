7068453bd94f8b285642fc35b775bbf8eb30b5c7
Hotakus <553070703@qq.com>
2021-08-13 05:11:12 +0800

    optimize project

---
+++ Source/HgmApp/HgmWiFi/HgmBT/HgmBT.h
@@ -0,0 +1,14 @@
+﻿#ifndef HELLGATEMONITOR_HGMBT_H
+#define HELLGATEMONITOR_HGMBT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_HGMBT_H
