87732efa6a16bdde1d4febf6191f4fddf18953fe
Hotakus <553070703@qq.com>
2021-08-13 19:12:36 +0800

    Finish the basic bluetooth framework

---
+++ Source/HgmSelfChecking/HgmSelfChecking.h
@@ -0,0 +1,14 @@
+﻿#ifndef HELLGATEMONITOR_HGMSELFCHECKING_H
+#define HELLGATEMONITOR_HGMSELFCHECKING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_HGMSELFCHECKING_H
