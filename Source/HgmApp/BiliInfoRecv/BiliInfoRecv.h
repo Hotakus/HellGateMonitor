c43c337b0669c4802b0591cb1c05b9242a9188d1
Hotakus <553070703@qq.com>
2021-08-17 00:55:59 +0800

    Complete the bilibili info get

---
+++ Source/HgmApp/BiliInfoRecv/BiliInfoRecv.h
@@ -0,0 +1,14 @@
+﻿#ifndef HELLGATEMONITOR_BILIINFORECV_H
+#define HELLGATEMONITOR_BILIINFORECV_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_BILIINFORECV_H
