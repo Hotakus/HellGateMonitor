858d9e9bae0c127c171a4d089151a7594f15d06f
Hotakus <553070703@qq.com>
2021-08-20 22:31:32 +0800

    feat(time info) Add file

---
+++ Source/HgmApp/TimeInfo/TimeInfo.h
@@ -0,0 +1,14 @@
+﻿#ifndef HELLGATEMONITOR_TIMEINFO_H
+#define HELLGATEMONITOR_TIMEINFO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_TIMEINFO_H
