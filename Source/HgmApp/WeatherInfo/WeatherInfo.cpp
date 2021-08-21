8a933af5bcae064d232d6b162596da0265bf8d1a
Hotakus <553070703@qq.com>
2021-08-22 07:26:39 +0800

    weather

---
+++ Source/HgmApp/WeatherInfo/WeatherInfo.cpp
@@ -0,0 +1,1 @@
+﻿#include "WeatherInfo.h"
---
+++ Source/HgmApp/WeatherInfo/WeatherInfo.h
@@ -0,0 +1,14 @@
+﻿#ifndef HELLGATEMONITOR_WEATHERINFO_H
+#define HELLGATEMONITOR_WEATHERINFO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_WEATHERINFO_H
