e2c833f758a5aa3864dbae1042d68300403c91e1
Hotakus <553070703@qq.com>
2021-09-24 10:50:21 +0800

    feat(Network) hardware data class

---
+++ Source/HgmApp/HardwareInfoRecv/HardwareNetData.cpp
@@ -0,0 +1,22 @@
+﻿/******************************************************************
+ * @file HardwareNetData.cpp
+ * @author Hotakus (...)
+ * @email ttowfive@gmail.com
+ * @brief ...
+ * @version 1.0
+ * @date 2021/9/23 20:12
+ * @copyright Copyright (c) 2021/9/23
+*******************************************************************/
+#include "HardwareNetData.h"
+#include "../HgmJsonUtil.h"
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+using namespace HgmApplication;
+using namespace HgmApplication::HgmJsonParseUtil;
+
+HardwareNetData hardwareNetData;
+
+
+
---
+++ Source/HgmApp/HardwareInfoRecv/HardwareNetData.h
@@ -0,0 +1,35 @@
+﻿/******************************************************************
+ * @file HardwareNetData.h
+ * @author Hotakus (...)
+ * @email ttowfive@gmail.com
+ * @brief ...
+ * @version 1.0
+ * @date 2021/9/23 20:12
+ * @copyright Copyright (c) 2021/9/23
+*******************************************************************/
+#ifndef HELLGATEMONITOR_HARDWARENETDATA_H
+#define HELLGATEMONITOR_HARDWARENETDATA_H
+
+#include "../HgmJsonUtil.h"
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+using namespace HgmApplication::HgmJsonParseUtil;
+
+namespace HgmApplication {
+	class HardwareNetData {
+	private:
+	public:
+
+
+
+
+		HardwareNetData();
+		~HardwareNetData();
+
+		void Set(HotakusDynamicJsonDocument& hdjd);
+	};
+}
+
+#endif //HELLGATEMONITOR_HARDWARENETDATA_H
