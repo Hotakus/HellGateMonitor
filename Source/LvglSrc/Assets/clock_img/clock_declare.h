b587acba8bf21a8ac9af2e9675b42942656f5fa0
Hotakus <553070703@qq.com>
2021-10-06 07:18:04 +0800

    arch(clock) clock img source file

---
+++ Source/LvglSrc/Assets/clock_img/clock_declare.h
@@ -0,0 +1,27 @@
+﻿/******************************************************************
+ * @file clock_declare.h
+ * @author Hotakus (...)
+ * @email ttowfive@gmail.com
+ * @brief ...
+ * @version 1.0
+ * @date 2021/10/6 6:39
+ * @copyright Copyright (c) 2021/10/6
+*******************************************************************/
+#ifndef HELLGATEMONITOR_CLOCK_DECLARE_H
+#define HELLGATEMONITOR_CLOCK_DECLARE_H
+
+
+#include <lvgl.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //HELLGATEMONITOR_CLOCK_DECLARE_H
